839add11456fad0e00cf64864fba3fadd1f93c86
souza.leandror@gmail.com <souza.leandror@gmail.com>
2020-02-03 22:11:48 +0100

    Update 70%

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:chats/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -7,14 +13,122 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  FirebaseUser _currentUser;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      _currentUser = user;
+    });
+  }
+
+  Future<FirebaseUser> _getUser() async {
+    if (_currentUser != null) return _currentUser;
+
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
+
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+
+      final AuthResult authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final FirebaseUser user = authResult.user;
+
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  void _sendMessage({String text, File imgFile}) async {
+    final FirebaseUser user = await _getUser();
+
+    if (user == null) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text("Nao foi possivel fazer o login. tenten novamente"),
+        backgroundColor: Colors.red,
+      ));
+    }
+
+    Map<String, dynamic> data = {
+      "uid": user.uid,
+      "senderName": user.displayName,
+      "senderPhotoUrl": user.photoUrl
+    };
+
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child("pasta")
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
+
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      print(url);
+      data["imgUrl"] = url;
+    }
+
+    if (text != null) data["text"] = text;
+
+    Firestore.instance.collection("menssages").add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Chat"),
         centerTitle: true,
       ),
-      body: TextComposer(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder(
+              stream: Firestore.instance.collection("menssages").snapshots(),
+              builder: (context, snapsnot) {
+                switch (snapsnot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+
+                  default:
+                    List<DocumentSnapshot> documents = snapsnot.data.documents;
+
+                    return ListView.builder(
+                      itemCount: documents.length,
+                      reverse: true,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                            title: Text(
+                          documents[index].data["text"],
+                        ));
+                      },
+                    );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage),
+        ],
+      ),
     );
   }
 }
--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,12 +1,28 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class TextComposer extends StatefulWidget {
+  TextComposer(this.sendMessage);
+
+  final Function({String text, File imgFile}) sendMessage;
+
   @override
   _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
   bool _isComposing = false;
+
+  final TextEditingController _controller = TextEditingController();
+
+  void _reset() {
+    _controller.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +32,16 @@ class _TextComposerState extends State<TextComposer> {
         children: <Widget>[
           IconButton(
             icon: Icon(Icons.photo_camera),
-            onPressed: () {},
+            onPressed: () async {
+              final File imgFile =
+                  await ImagePicker.pickImage(source: ImageSource.camera);
+              if (imgFile == null) return;
+              widget.sendMessage(imgFile: imgFile);
+            },
           ),
           Expanded(
             child: TextField(
+              controller: _controller,
               decoration:
                   InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
               onChanged: (text) {
@@ -27,12 +49,20 @@ class _TextComposerState extends State<TextComposer> {
                   _isComposing = text.isNotEmpty;
                 });
               },
-              onSubmitted: (text) {},
+              onSubmitted: (text) {
+                widget.sendMessage(text: _controller.text);
+                _reset();
+              },
             ),
           ),
           IconButton(
             icon: Icon(Icons.send),
-            onPressed: (_isComposing ? () {} : null),
+            onPressed: (_isComposing
+                ? () {
+                    widget.sendMessage(text: _controller.text);
+                    _reset();
+                  }
+                : null),
           ),
         ],
       ),
